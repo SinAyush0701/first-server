c062ff7f1620c029d03e488a699286391784be31
SinAyush0701 <iayushsingh23@gmail.com>
2023-08-03 01:51:16 +0530

    created a server with get & post

--- server.js
+++ server.js
@@ -4,10 +4,38 @@ const http= require("http");
 
 const port= 8081;
 
+const toDoList= ["Need to learn", " Need to code"];
+
 http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<h4>Hello, this is from my new server</h4>");
-    res.end();
+  //  res.writeHead(200, {"Content-Type": "text/html"});
+  //  res.write("<h4>Hello, this is from my new server</h4>");
+  //  res.end();
+      const {method, url}= req;
+ //   console.log(method, url);
+ //   res.end();
+      if(url=== "/todos"){
+        // http://localhost:8081/todos
+        if(method === "GET") {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(toDoList.toString());
+            } else if(method === "POST"){
+                let body= "";
+                req.on('error', (err) => {
+                    console.log(err);
+                }).on('data', (chunk) => {
+                    body += chunk;
+                  //  console.log(chunk);
+                }).on('end', () => {
+                    body= JSON.parse(body);
+                    console.log("body data", body);
+                });
+            } else {
+            res.writeHead(501);
+           }    
+        } else {
+            res.writeHead(404);
+        }
+      res.end();
 }).listen(port, () => {
     console.log(`My NodeJS server started on port ${port}`);
 });
